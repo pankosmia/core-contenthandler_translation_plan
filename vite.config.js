c9e087fa76f20a2cff5d38e6d60c7ebbf2517e46
lmeline <loisemel@hotmail.fr>
2026-08-03 11:25:39 +0200

    ready to puslish

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,57 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5174, // Unique port for new_project
-    strictPort: true,
-    host: true,
-    cors: true,
-    origin: "http://localhost:8000",
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:19119", // Backend server
-        changeOrigin: true, // Ensure the request appears to come from the frontend server
+export default defineConfig(({ mode }) => {
+  const isMuncher = mode === "isMuncher";
+  return {
+    plugins: [react()],
+    server: {
+      port: 5174, // Unique port for new_project
+      strictPort: true,
+      host: true,
+      cors: true,
+      origin: "http://localhost:8000",
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:19119", // Backend server
+          changeOrigin: true, // Ensure the request appears to come from the frontend server
+        },
       },
     },
-  },
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-  base: "/clients/core-contenthandler_translation_plan/",
+    build: isMuncher
+      ? {
+          outDir: "src/components/translationPlanMuncher/munchersPackageExport",
+          emptyOutDir: true,
+          sourcemap: true,
+
+          rollupOptions: {
+            external: ["react", "react-dom", "react-router-dom"],
+            output: {
+              name: "translationPlanRcl",
+              globals: {
+                react: "React",
+                "react-dom": "ReactDOM",
+                "react-router-dom": "ReactRouterDOM",
+              },
+            },
+          },
+
+          lib: {
+            entry: path.resolve(
+              __dirname,
+              "./src/components/translationPlanMuncher/muncher/index.js",
+            ),
+            name: "pankosmiaRcl",
+            fileName: (format) => `translation_plan-muncher-rcl.${format}.js`,
+          },
+        }
+      : {
+          outDir: "build",
+          emptyOutDir: true,
+          sourcemap: true,
+        },
+    base: "/clients/core-contenthandler_translation_plan/",
+  };
 });
